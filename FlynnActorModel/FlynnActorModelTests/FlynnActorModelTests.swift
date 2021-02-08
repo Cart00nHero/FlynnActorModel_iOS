1b035bf88b26ff52b4bb8d76a3314eb4bae74b46
Cart00nHero8 <Cart00nHero8@gmail.com>
2021-02-08 22:13:10 +0800

    Upload initial files

---
+++ FlynnActorModel/FlynnActorModelTests/FlynnActorModelTests.swift
@@ -0,0 +1,33 @@
+//
+//  FlynnActorModelTests.swift
+//  FlynnActorModelTests
+//
+//  Created by YuCheng on 2021/2/8.
+//
+
+import XCTest
+@testable import FlynnActorModel
+
+class FlynnActorModelTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
